90abdf8f460339311cebf7cb5569c7df3eb31dc2
Masashi Kudo <ma34@MasashinoMacBook-Pro.local>
2018-03-26 17:47:19 +0900

    test

---
+++ PhotoMatchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoMatchApp
+//
+//  Created by Masashi Kudo on 2018/03/26.
+//  Copyright © 2018年 Masashi Kudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
